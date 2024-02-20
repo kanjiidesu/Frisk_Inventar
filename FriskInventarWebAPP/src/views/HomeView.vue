dd668e9ffeaf05e49970a96f58f319f2f208ce07
karina.laursen <karina.laursen@ssi-schaefer.com>
2024-02-20 10:23:38 +0100

    img fix

--- FriskInventarWebAPP/src/views/HomeView.vue
+++ FriskInventarWebAPP/src/views/HomeView.vue
@@ -5,7 +5,7 @@ import { getFridgeData, postFridgeData } from '../../api';
 
 <template>
     <div class="home">
-      <img src="C:\Users\Karina.Laursen\Documents\Skole\HF5\Tværfagligt_Projekt\Projekt\Img\fridge.png" style="float:right; padding-right: 100px; padding-top: 100px;">
+      <img src="../assets/Img/fridge.png" style="float:right; padding-right: 100px; padding-top: 100px;">
     <h1 class="frontpageh1">Velkommen</h1>
     <p>Her kan du komme godt i gang med at få styr på dit køleskab.</p>
     <p v-for="(fridge, index) in fridgeData" :key="index">{{ `${index+1}: ${fridge.fridgeName}` }}</p>
